b40dbbbfd970c9fd64d6208bee5cc86b38f9f7f2
BrayanGB09 <bgutierrez@fwdcostarica.com>
2024-08-07 16:14:06 -0600

    cambios

--- src/js/gato.js
+++ src/js/gato.js
@@ -3,31 +3,45 @@ const contenedor = document.getElementsByClassName("contenedor")
     for (let index = 0; index < contenedor.length; index++) {
 
         contenedor[index].addEventListener("click", function () {
-
-            contenedor[index].innerHTML = "X";
-            
-            
+            if (contenedor[index].textContent === "") {
+                
+            contenedor[index].textContent = "X";
+        
+      
             let lista = []
 
             for (let index = 0; index < contenedor.length; index++) {
 
                 if (contenedor[index].textContent === "") {
 
-                    lista.push(index)
+                    lista.push(contenedor[index])
                   
                 }
-               
             }
+            
+                if (lista.length > 0) {
 
-            let numeroAleatorio = Math.floor(Math.random()*lista.length);
+                    let numeroAleatorio = Math.floor(Math.random()*lista.length);
 
-            let contenido = lista[numeroAleatorio]
+                    lista[numeroAleatorio].textContent = "O"
     
-           contenedor[contenido].innerHTML = "O"  
+                }
+            }    
+            
+            const victoria = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8], 
+                [0, 4, 8],
+                [2, 4, 6]
+            ]; 
+
+          
+           
+
         })
-
     }
-
-
-
-
+   
