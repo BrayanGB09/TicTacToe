52fc3810f30b5ca0d116085aff17af73a443661e
BrayanGB09 <bgutierrez@fwdcostarica.com>
2024-08-08 15:47:50 -0600

    cambios

--- src/js/gato.js
+++ src/js/gato.js
@@ -1,12 +1,14 @@
 const contenedor = document.getElementsByClassName("contenedor")
+const texto = document.getElementById("texto")
 
     for (let index = 0; index < contenedor.length; index++) {
-
+      
         contenedor[index].addEventListener("click", function () {
+           
             if (contenedor[index].textContent === "") {
                 
-            contenedor[index].textContent = "X";
-        
+            contenedor[index].textContent = "X"
+
       
             let lista = []
 
@@ -25,23 +27,33 @@ const contenedor = document.getElementsByClassName("contenedor")
 
                     lista[numeroAleatorio].textContent = "O"
     
-                }
-            }    
+                }  
+
+                const celdas = [  
+                    [0, 1, 2],
+                    [3, 4, 5],
+                    [6, 7, 8],
+                    [0, 3, 6],
+                    [1, 4, 7],
+                    [2, 5, 8],
+                    [0, 4, 8],
+                    [2, 4, 6]
+                ];
             
-            const victoria = [
-                [0, 1, 2],
-                [3, 4, 5],
-                [6, 7, 8],
-                [0, 3, 6],
-                [1, 4, 7],
-                [2, 5, 8], 
-                [0, 4, 8],
-                [2, 4, 6]
-            ]; 
-
-          
-           
-
+                for (let index = 0; index < celdas.length; index++) {
+    
+                    let condicion = celdas[index]
+                    
+                    let celdaA = condicion[0];
+                    let celdaB = condicion[1];
+                    let celdaC = condicion[2];
+    
+                    if (celdaA[index].textContent === "X" && celdaB[index].textContent === "X" && celdaC[index].textContent === "X") {
+                        texto.textContent = "Has ganado";
+                
+                    }
+                }
+            }
         })
     }
    
